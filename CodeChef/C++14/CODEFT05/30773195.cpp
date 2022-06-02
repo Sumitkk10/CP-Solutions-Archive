4e2ff5bca3ffde63f04804d1b50b77404eea8de4
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:33:12 +0530

    Committing files

---
+++ CodeChef/C++14/CODEFT05/30773195.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int main(){
+    fast;
+    ll n;
+    cin >> n;
+    cout << 1;
+    return 0;
+} 
