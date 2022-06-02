f2ffc2d265e75e05f2bc35cb90ba64f62e9a0ce5
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:30:46 +0530

    Committing files

---
+++ CodeChef/C++14/CODSTAN1/38363864.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+
+int main(){
+	fast;
+	ll n;
+	cin >> n;
+	map<char, int> mp;
+	string s;
+	cin >> s;
+	for(auto k : s)
+		mp[k]++;
+	if(mp['V'] > mp['H']) cout << "Virat\n";
+	else if(mp['V'] < mp['H']) cout << "Harshit\n";
+	else cout << "Friendship\n";
+	return 0;
+}
