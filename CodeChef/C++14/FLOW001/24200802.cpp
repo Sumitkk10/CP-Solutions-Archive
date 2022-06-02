089faa1b4e88f05856e315de9a9198bda225d8f1
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:34:15 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW001/24200802.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, c, b;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cin >> b >> c;
+        cout << b + c << '\n';
+    }
+  return 0;
+}
