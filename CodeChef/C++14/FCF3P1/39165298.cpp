0aeda5610ec9d6936b5bf19eb328edabe97b1946
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:30:29 +0530

    Committing files

---
+++ CodeChef/C++14/FCF3P1/39165298.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 500;
+const int MOD = 1e9 + 7;
+
+void solve() {
+	int n;
+	long long int k;
+	cin >> n >> k;
+	int a[n];
+	long long int sum = 0;
+	for (int i = 0; i < n; ++i){
+		cin >> a[i];
+		sum = (sum + a[i]) % MOD;
+	}
+	if((k / n) != 0)
+		sum = (sum * (k / n)) % MOD;
+	else
+		sum = 0;
+	int j = k % n;
+	if((k / n) % 2){
+		for(int i = n - 1; i > (n - j - 1); --i)
+			sum = (sum + a[i]) % MOD; 
+		cout << sum << "\n";
+	}
+	else{
+		for(int i = 0; i < j; ++i)
+			sum = (sum + a[i]) % MOD;
+		cout << sum << "\n";
+	}
+}
+
+int main() {
+	fast;
+	int t = 1;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
