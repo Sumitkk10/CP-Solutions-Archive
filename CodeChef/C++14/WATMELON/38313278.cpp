0aae73a2900615a7a8ab36c356611703a5a04002
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:30:51 +0530

    Committing files

---
+++ CodeChef/C++14/WATMELON/38313278.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+//using namespace __gnu_pbds;
+
+#define ll long long int
+#define boost ios::sync_with_stdio(false); cin.tie(0)
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define fd(i,a,b) for(i=a-1;i>=b;i--)
+#define rep(i,n) f(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define inf (1000*1000*1000+5)
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define tri2 pair<pii,int>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define viii2 vector<tri2>
+#define umapi unordered_map<int,int>
+#define mapi map<int,int>
+#define useti unordered_set<int>
+#define seti set<int>
+#define pqueue priority_queue< int >
+#define pdqueue priority_queue< int,vi ,greater< int > >
+#define coutp(i) cout << fixed << setprecision(i)
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define endl "\n"
+void solve(){
+  int n;
+  cin>>n;
+  ll sum = 0;
+  ll arr[11];
+  rep(i,n) cin>>arr[i];
+  rep(i,n) sum+=arr[i];
+//   cout<<sum<<endl;
+  if(sum >= 0) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+  	
+}
+main()
+{
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+   	solve();
+   }
+}
