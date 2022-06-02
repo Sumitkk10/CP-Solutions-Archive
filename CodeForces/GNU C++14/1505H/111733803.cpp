27c8c0cbccf7b65d8e7394be80f769f6118891f7
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 18:44:19 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1505H/111733803.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+using namespace std;
+const int N = 1e3 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+	int x;
+	cin >> x;
+	cout << "contest/171" << "\n"; 
+}
+
+int main(){
+    fast;
+	int t = 1;
+	// cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
