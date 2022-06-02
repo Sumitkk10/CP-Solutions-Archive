06f23ede096221a99f820dcac92b931712d1d42d
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:31:05 +0530

    Committing files

---
+++ CodeChef/C++14/ECAUG203/37379879.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+
+int main(){
+	fast;
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n; cin >> n;
+		cout << 2*((ll)(sqrtl(n/2))) << "\n";
+	}
+	return 0;
+}
