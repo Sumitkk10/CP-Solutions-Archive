cf9fdd32df87b8b5d586bf89aa62a33fd459de77
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 17:27:41 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1579D/130139415.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    int a[n + 1];
+    multiset<pair<int, int> > s;
+    for(int i = 1; i <= n; ++i){
+        cin >> a[i];
+        if(a[i] > 0)
+            s.insert({a[i], i});
+    }
+    vector<pair<int, int> > ans;
+    while(s.size() > 1){
+        pair<int, int> p = *s.begin();
+        pair<int, int> pp = *s.rbegin();
+        s.erase(s.begin());
+        s.erase(--s.end());
+        ans.push_back({p.second, pp.second});
+        p.first--;
+        pp.first--;
+        if(p.first > 0)
+            s.insert(p);
+        if(pp.first > 0)
+            s.insert(pp);
+    }
+    cout << ans.size() << "\n";
+    for(auto k : ans)
+        cout << k.first << ' ' << k.second << "\n";
+}
+
+int main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
