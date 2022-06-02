559d007b5856f689086aa05dc8916369cb6dc1ed
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:33:47 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/29259306.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+	long long int t, k = 0;
+	while(k != 42){
+		cin >> k;
+		if(k != 42) 
+			cout << k << '\n';
+	}
+	return 0;
+}
