b797dce77d6c6079c509b5ca171f17b1f1008cbf
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:23:57 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/31016515.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+int main(){
+    fast;
+    ll n;
+    while(1){
+        cin >> n;
+        if(n != 42)
+            cout << n << '\n';
+        else
+            break;
+    }
+    return 0;
+} 
