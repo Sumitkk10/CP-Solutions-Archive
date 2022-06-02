717c89713545833f26124f8755808a72c8bfcc12
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:29:42 +0530

    Committing files

---
+++ CodeChef/C++14/ITGUY14/40509403.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+	int n;
+	cin >> n;
+	int a[n], ans[n];
+	for(int i = 0; i < n; ++i){
+		cin >> a[i];
+		ans[i] = 0;
+	}
+	for(int i = 0; i < n; ++i)
+		if(ans[a[i]] == 0)
+			ans[a[i]] = a[i];
+	for(int i = 0; i < n; ++i)
+		cout << ans[i] << ' ';
+	cout << "\n";
+}
+
+int main(){
+	fast;
+	int t = 1;
+	cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
