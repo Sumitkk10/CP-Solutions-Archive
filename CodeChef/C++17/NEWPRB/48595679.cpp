d1376796d2a42635fa553ddf2e7120184700afd1
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:23:13 +0530

    Committing files

---
+++ CodeChef/C++17/NEWPRB/48595679.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    long long int cur = 3, ct = 4, ok = 1;
+    while(ok < n){
+        cur += ct;
+        ct += 2;
+        ++ok;
+    }
+    cout << cur << '\n';
+}
+
+int main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
