747f2feb02d43ed02cd2ed6c658d23865d519a02
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 18:44:38 +0530

    Committing files

---
+++ CodeForces/GNU C++14/23B/100619358.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int main(){
+	fast;
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		cout << max(n - 2, 0) << '\n'; 
+	}
+	return 0;
+}
+//								Worst Problem Ever!!
