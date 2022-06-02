22c90ad6267f7c5c3b9de65c4c2c6fc90c53447f
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:26:23 +0530

    Committing files

---
+++ CodeChef/C++14/ECAPR202/32303711.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+const int N = 1000000 + 5;
+
+int main(){
+    fast;
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        ll ans = 2*n;
+        ll ans1 = ans*ans;
+        cout << ans1/2 << '\n'; 
+    }
+    return 0;
+}
+//                                     #!!{*[$um1t_Kum@r]*}!!#
