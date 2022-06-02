f9a439329dc3aa830652b8eea4e31d06d5d821c5
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:26:40 +0530

    Committing files

---
+++ CodeChef/C++14/ZCOSCH/27689219.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+	int t, i, n, m, s;
+	cin >> t;
+	if(t <= 50)
+		cout << 100;
+	else if (t > 50 && t <= 100)
+		cout << 50;
+	else
+		cout << 0;
+	
+	return 0;
+}
