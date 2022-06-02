24c77245250a1463dd75de3d43086506ea2201bd
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 18:10:32 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1455A/99994398.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 3e6 + 1;
+const int MOD = 1e9 + 7;
+vector<int> graph[N];
+
+void solve(){
+	string s;
+	cin >> s;
+	cout << s.size() << "\n";
+}
+
+int main(){
+	fast;
+	int t = 1;
+	cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
