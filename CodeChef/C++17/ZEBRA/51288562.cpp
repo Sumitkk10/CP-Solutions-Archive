3a8c206e6b8e34bf6f422299c136fd0e4206552c
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:22:43 +0530

    Committing files

---
+++ CodeChef/C++17/ZEBRA/51288562.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    int jmp = 0;
+    char cur = s[0];
+    for(int i = 1; i < s.size(); ++i){
+        if(s[i] != cur){
+            ++jmp;
+            cur = s[i];
+        }
+    }
+    if(jmp < k){
+        cout << "-1\n";
+        return;
+    }
+    if(k % 2 == 0){
+        for(int i = s.size() - 1; i >= 0; --i){
+            if(s[i] == s[0]){
+                cout << i + 1 << '\n';
+                return;
+            }
+        }
+        cout << "-1\n";
+        return;
+    }
+    for(int i = s.size() - 1; i >= 0; --i){
+        if(s[i] != s[0]){
+            cout << i + 1 << '\n';
+            return;
+        }
+    }
+    cout << "-1\n";
+    return;
+}
+
+int main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
