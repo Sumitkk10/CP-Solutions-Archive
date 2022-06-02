d4d5fdd665f1a945d33b24ac30cd4701cc65b847
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 17:10:31 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1679D/157234050.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define F first
+#define S second
+#define ll long long
+using namespace std;
+const int N = 1e6, MOD = 1e9 + 7;
+long long n, m, k, val[N], col[N], mx, dpp[N], indegree[N], LPD[N];
+vector<int> g[N];
+bool vis[N], cycle;
+queue<int> q;
+
+void dfs(int node, long long mid, long long dis){
+	col[node] = 1;
+	for(auto kk : g[node]){
+		if(val[kk] > mid) 
+			continue;
+		if(col[kk] == 1)
+			cycle = true;
+		else if(!col[kk])
+			dfs(kk, mid, dis + 1);
+	}
+	col[node] = 2;
+}
+
+void topo(int mid){
+	while(!q.empty()){
+		int x = q.front();
+		q.pop();
+		for(auto k : g[x]){
+			if(!vis[k] and val[k] <= mid){
+				indegree[k]--;
+				if(indegree[k] == 0){
+					q.push(k);
+					vis[k] = true;
+				}
+				LPD[k] = max(LPD[k], LPD[x] + 1);
+			}
+		}
+	}
+}
+
+void dfss(int source, int mid){
+	vis[source] = 1;
+	for(auto kk : g[source]){
+		indegree[kk]++;
+		if(!vis[kk] and val[kk] <= mid)
+			dfss(kk, mid);
+	}
+}
+
+bool check(long long mid){
+	for(int i = 1; i <= n; ++i){
+		col[i] = 0;
+		vis[i] = 0;
+                indegree[i] = 0;
+                LPD[i] = 0;
+	}
+	cycle = 0;
+	for(int i = 1; i <= n; ++i){
+		if(!col[i] and val[i] <= mid){
+			dfs(i, mid, 1);
+			if(cycle)
+				return true;
+		}
+	}
+	for(int i = 1; i <= n; ++i)
+		if(!vis[i] and val[i] <= mid)
+			dfss(i, mid);
+	for(int i = 1; i <= n; ++i)
+		vis[i] = 0;
+	for(int i = 1; i <= n; ++i){
+		if(!indegree[i] and val[i] <= mid){
+			q.push(i);
+			vis[i] = true;
+		}
+	}
+	topo(mid);
+	long long ans = 0;
+	for(int i = 1; i <= n; ++i){
+	     if(val[i] <= mid)
+		     ans = max(ans, LPD[i] + 1);
+	}
+	return (ans >= k);
+}
+
+void solve(){
+	cin >> n >> m >> k;
+	for(int i = 1; i <= n; ++i)
+		cin >> val[i];
+	for(int i = 0; i < m; ++i){
+		int u, v;
+		cin >> u >> v;
+		g[u].pb(v);
+	}
+	long long low = 1, high = 1e9, ans = -1;
+	while(low <= high){
+		long long mid = (low + (high - low) / 2);
+		if(check(mid)){
+			ans = mid;
+			high = mid - 1;
+		}
+		else
+			low = mid + 1;
+	}
+	cout << ans << '\n';
+}
+
+int main(){
+	fast;
+	int t = 1;
+	// cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
