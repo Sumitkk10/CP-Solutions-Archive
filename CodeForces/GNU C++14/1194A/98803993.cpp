92007e298ab3732fe880f7ce3704404f742783cf
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 18:11:12 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1194A/98803993.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+     int a, b;
+     cin >> a >> b;
+     cout << 2 * b << '\n';
+}
+
+int main(){
+      int t; cin >> t;
+      while(t--)
+          solve();
+      return 0;
+}
