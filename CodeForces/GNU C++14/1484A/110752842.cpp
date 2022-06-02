7d1da9e8c7fa8b1a538188af09ce8329c5aed550
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 18:01:30 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1484A/110752842.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int N = 1e6 + 5;
+
+void solve(){
+	int a, b;
+	cin >> a >> b;
+	cout << a * b << "\n";
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
