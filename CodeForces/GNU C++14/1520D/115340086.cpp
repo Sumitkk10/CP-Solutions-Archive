d26473ab42c61c442fe91faa88ee8c726f364b8b
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 17:52:56 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1520D/115340086.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    long long int a[n + 1];
+    map<int, int> mp;
+    for(int i = 1; i <= n; ++i){
+        cin >> a[i];
+        mp[a[i] - i]++;
+    }
+    long long  ans = 0;
+    for(auto k : mp){
+         long long ct = k.second;
+        ans += (ct * (ct - 1)) / 2;
+     }
+     cout << ans << '\n';
+}
+
+int main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
