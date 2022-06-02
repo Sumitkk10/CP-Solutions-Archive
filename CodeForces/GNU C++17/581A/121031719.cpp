ada72affa384cd6a73c66571804d5abf9939a621
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 17:44:14 +0530

    Committing files

---
+++ CodeForces/GNU C++17/581A/121031719.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+void solve(){
+	int a, b;
+	cin >> a >> b;
+	if(a > b)
+		swap(a, b);
+	cout << min(a, b) << ' ' << (b - a) / 2 << '\n';
+}
+ 
+int main() {
+	fast;
+	int t = 1;
+ 	// cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}    
