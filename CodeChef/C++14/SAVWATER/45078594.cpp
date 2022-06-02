bccf37860d9f5a6e9b72824c8a438d2674db7ee7
Sumit Kumar <messikumar66@gmail.com>
2022-06-02 16:29:12 +0530

    Committing files

---
+++ CodeChef/C++14/SAVWATER/45078594.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if((a * (b + c/2)) <= d){
+		cout << "YES\n";
+		return;
+	}
+	cout << "NO\n";
+}
+
+int main(){
+	fast;
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
